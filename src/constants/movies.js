e7ed48b4047eb390e8591a5f0cbf2fd851827229
olimpiamarin <olimpiamarin84@gmail.com>
2017-05-30 00:33:53 +0300

    Create components and constants folder - array of object movies.js

---
+++ src/constants/movies.js
@@ -0,0 +1,10 @@
+var movies = [
+  {title:"World War Z", year:"Year: 2013"},
+  {title:"Salt", year:"Year: 2010"},
+  {title:"50/50", year:"Year: 2013"},
+  {title:"Shrek", year:"Year: 2001"},
+  {title:"Prometheus", year:"Year: 2012"},
+  {title:"The Prestige", year:"Year: 2006"},
+  {title:"A Series of Unfortunate Events", year:"Year: 2004"},
+  {title:"Up", year:"Year: 2009"}
+];
